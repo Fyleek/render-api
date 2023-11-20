9a1e32816eacf075e07fbfda88ec66a580d6d056
Decloquement Oscar <odecloquement@gmail.com>
2023-11-20 09:50:34 +0100

    Security Hotspots Fix

--- logging_config.py
+++ logging_config.py
@@ -1,58 +1,27 @@
 import logging
 
-# Create a logger object for your application
+
+def configure_file_handler(log_file, level):
+    handler = logging.FileHandler(log_file)
+    handler.setLevel(level)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    return handler
+
+
+# Logger for the app
 logger = logging.getLogger("render-api")
+logger.setLevel(logging.DEBUG)
 
-# Set the global logging level (can be adjusted as needed)
-logging.basicConfig(level=logging.INFO)
+# Add handlers with specific log level
+logger.addHandler(configure_file_handler("debug.log", logging.DEBUG))
+logger.addHandler(configure_file_handler("info.log", logging.INFO))
+logger.addHandler(configure_file_handler("warning.log", logging.WARNING))
+logger.addHandler(configure_file_handler("error.log", logging.ERROR))
+logger.addHandler(configure_file_handler("critical.log", logging.CRITICAL))
 
-# Configure a file handler to log DEBUG-level logs to a debug.log file
-debug_file_handler = logging.FileHandler("debug.log")
-debug_file_handler.setLevel(logging.INFO)
-
-# Create a formatter to customize the log message format
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-debug_file_handler.setFormatter(formatter)
-
-# Add the file handler to the logger for DEBUG-level messages
-logger.addHandler(debug_file_handler)
-
-# Configure a file handler to log INFO-level logs to an info.log file
-info_file_handler = logging.FileHandler("info.log")
-info_file_handler.setLevel(logging.INFO)
-
-# Add the formatter to the file handler
-info_file_handler.setFormatter(formatter)
-
-# Add the file handler to the logger for INFO-level messages
-logger.addHandler(info_file_handler)
-
-# Configure a file handler to log WARNING-level logs to a warning.log file
-warning_file_handler = logging.FileHandler("warning.log")
-warning_file_handler.setLevel(logging.WARNING)
-
-# Add the formatter to the file handler
-warning_file_handler.setFormatter(formatter)
-
-# Add the file handler to the logger for WARNING-level messages
-logger.addHandler(warning_file_handler)
-
-# Configure a file handler to log ERROR-level logs to an error.log file
-error_file_handler = logging.FileHandler("error.log")
-error_file_handler.setLevel(logging.ERROR)
-
-# Add the formatter to the file handler
-error_file_handler.setFormatter(formatter)
-
-# Add the file handler to the logger for ERROR-level messages
-logger.addHandler(error_file_handler)
-
-# Configure a file handler to log CRITICAL-level logs to a critical.log file
-critical_file_handler = logging.FileHandler("critical.log")
-critical_file_handler.setLevel(logging.CRITICAL)
-
-# Add the formatter to the file handler
-critical_file_handler.setFormatter(formatter)
-
-# Add the file handler to the logger for CRITICAL-level messages
-logger.addHandler(critical_file_handler)
+# Optinal handler for console logs
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+logger.addHandler(console_handler)
